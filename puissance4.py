c040f383a965844bf69f2bd02fd6f03fa2fa9508
Anis <36997365+uvsq22401387@users.noreply.github.com>
2025-03-13 12:54:02 +0100

    Mise en place du canvas

--- puissance4.py
+++ puissance4.py
@@ -5,5 +5,10 @@ import random
 
 root = tk.Tk()
 root.title("Puissance 4")
+jeu_actif = True
 
+
+
+canvas = tk.Canvas(root, width=colonnes * dim_case, height=lignes * dim_case, bg="#2C3E50")
+canvas.pack()
 root.mainloop()
