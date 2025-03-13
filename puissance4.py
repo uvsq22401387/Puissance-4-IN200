329dd5965c6adaf562716cd8692e945703c5c2a3
Ghofrane MakboulM <ghofrane.makboulm@ens.uvsq.fr>
2025-03-13 13:29:33 +0100

    Ajout de la gestion des joueurs

--- puissance4.py
+++ puissance4.py
@@ -5,15 +5,30 @@ import random
 
 root = tk.Tk()
 root.title("Puissance 4")
+
 jeu_actif = True
 lignes = 6
 colonnes = 7
 dim_case = 80
 grille = [[None] * colonnes for _ in range(lignes)]
 
+joueurs = [
+    {"nom": "Joueur 1", "couleur": "red"},
+    {"nom": "Joueur 2", "couleur": "yellow"}
+]
 
-#gestion des joueurs à ajouter par ghofrane pour que 'couleur' soit reconnu
+def matchmaking():
+    joueur_actuel = random.choice(joueurs)
+    return joueur_actuel
+joueur_actuel = matchmaking()
 
+
+def selection_joueur():
+    global joueur_actuel
+    if joueur_actuel == joueurs[0]:
+        joueur_actuel = joueurs[1]
+    else:
+        joueur_actuel = joueurs[0] 
 
 def afficher_grille():
     canvas.delete("all")
@@ -33,4 +48,5 @@ def afficher_grille():
 
 canvas = tk.Canvas(root, width=colonnes * dim_case, height=lignes * dim_case, bg="#2C3E50")
 canvas.pack()
+afficher_grille()
 root.mainloop()
