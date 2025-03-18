017516a8f56c0d46628528cf3e1eec4a2b893c4f
Detolle Pierre <pierre.detolle-laposse@ens.uvsq.fr>
2025-03-18 11:17:53 +0100

    update

--- puissance4.py
+++ puissance4.py
@@ -12,6 +12,7 @@ grille = [[None] * colonnes for _ in range(lignes)]
 joueurs = [
     {"nom": "Joueur 1", "couleur": "red"},
     {"nom": "Joueur 2", "couleur": "yellow"}]
+canvas = tk.Canvas(root, width=colonnes * dim_case, height=lignes * dim_case, bg="#2C3E50")
 
 def matchmaking():
     joueur_actuel = rd.choice(joueurs)
@@ -30,8 +31,10 @@ def afficher_grille():
     canvas.delete("all")
     for i in range(lignes):
         for j in range(colonnes):
-            x1, y1 = j * dim_case + 5, i * dim_case + 5
-            x2, y2 = x1 + dim_case - 10, y1 + dim_case - 10
+            x1= j * dim_case + 5
+            y1 = i * dim_case + 5
+            x2= x1 + dim_case - 10
+            y2 = y1 + dim_case - 10
             if grille[i][j] is None:
                 couleur = "white"
             else:
@@ -138,7 +141,7 @@ def recommencer(fenetre2):#si on avait utilisé global ca aurait pas actualisé 
     afficher_grille()
 
 
-canvas = tk.Canvas(root, width=colonnes * dim_case, height=lignes * dim_case, bg="#2C3E50")
+
 canvas.pack()
 afficher_grille()
 canvas.bind("<Button-1>", interagir_jeu)
