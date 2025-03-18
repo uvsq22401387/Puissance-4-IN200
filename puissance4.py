2b9631bca0bc3216c777395a7ebd41aac4111ed5
pierrot <pierre.detolle-laposse@ens.uvsq.fr>
2025-03-18 01:49:36 +0100

    creation de la fonction verifie victoire

--- puissance4.py
+++ puissance4.py
@@ -50,18 +50,71 @@ def interagir_jeu(event):
             if grille[i][colonne] is None:
                 grille[i][colonne] = joueur_actuel
                 afficher_grille()
-                #if verifier_victoire(ligne, colonne):
-                    #fenetre_congrats(joueur_actuel)
-                    #jeu_actif = False
-                    #return
+                if verifier_victoire(i, colonne):
+                    fenetre_congrats(joueur_actuel)
+                    jeu_actif = False
+                    return
                 selection_joueur()
                 return
+    
 
-#def verifier_victoire():
-#il manque le travail de pierre faut qu'il se bouge :)
-    #pass
+def verifier_vertical(ligne, colonne, couleur):
+    compteur = 0
+    for k in range(-3, 4):
+        if 0 <= (ligne + k) and (ligne + k) < lignes:
+            if couleur == grille[ligne+k][colonne]:
+                compteur += 1
+            else:
+                compteur = 0
+            if compteur == 4:
+                return True
+        
+    return False
+
+
+def verifier_horizontal(ligne, colonne, couleur):
+    compteur = 0
+    for k in range(-3,4):
+        if 0 <= (colonne + k) and (colonne + k) < colonnes:
+            if couleur == grille[ligne][colonne+k]:
+                compteur += 1
+            else:
+                compteur = 0
+            if compteur == 4:
+                return True
+    return False
+
+def verifier_diagonale(ligne, colonne, couleur):
+    compteur = 0
+    for k in range(-3, 4):
+        if (0 <= (colonne + k) and (colonne + k) < colonnes) and (0 <= (ligne + k) and (ligne + k) < len(grille)):
+            if couleur == grille[ligne+k][colonne+k]:
+                compteur += 1
+            else:
+                compteur = 0
+            if compteur == 4:
+                return True 
+    compteur = 0
+    for k in range(-3, 4):
+        if (0 <= (colonne + k) and (colonne + k) < colonnes) and (0 <= (ligne - k) and (ligne - k) < len(grille)):
+            if couleur == grille[ligne-k][colonne+k]:
+                compteur += 1
+            else:
+                compteur = 0
+            if compteur == 4:
+                return True 
+    return False
+
+
+def verifier_victoire(ligne, colonne):
+    couleur = joueur_actuel
+    if verifier_vertical(ligne, colonne, couleur) or verifier_horizontal(ligne, colonne, couleur) or verifier_diagonale(ligne, colonne, couleur):
+        return True
+    return False
+
 
 def fenetre_congrats(joueur):
+
     global jeu_actif
     jeu_actif = False
     
